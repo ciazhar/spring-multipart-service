7bebe30867c9f750ad399052b96316bb94e84c3c
ciazhar <daisetsunakama@gmail.com>
2017-11-07 17:26:54 +0700

    initial commit

---
+++ src/main/kotlin/com/ciazhar/springwebfluxmultipart/SpringWebfluxMultipartApplication.kt
@@ -0,0 +1,11 @@
+package com.ciazhar.springwebfluxmultipart
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SpringWebfluxMultipartApplication
+
+fun main(args: Array<String>) {
+    runApplication<SpringWebfluxMultipartApplication>(*args)
+}

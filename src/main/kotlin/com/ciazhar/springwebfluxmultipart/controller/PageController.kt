f050c3bc721dc5cf6dbbf10d095d4bd0f8676660
ciazhar <daisetsunakama@gmail.com>
2017-11-07 18:37:13 +0700

    make controller

---
+++ src/main/kotlin/com/ciazhar/springwebfluxmultipart/controller/PageController.kt
@@ -0,0 +1,41 @@
+package com.ciazhar.springwebfluxmultipart.controller
+
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.PathVariable
+import java.nio.file.Files
+import java.nio.file.Paths
+import kotlin.streams.toList
+
+/**
+ * Created by ciazhar on 11/7/17.
+ *
+ * [ Documentation Here ]
+ */
+
+@Controller
+class PageController(@Value("\${video.location}") val videoLocation: String) {
+
+    @GetMapping
+    fun index(model: Model): String {
+        // getting all of the files in video folder
+        val videos = Files.list(Paths.get(videoLocation)).map { it.fileName.toString() }.toList()
+        model.addAttribute("videos", videos)
+        return "index"
+    }
+
+    @GetMapping("/{videoName}")
+    fun video(@PathVariable videoName: String, model: Model): String {
+        model.addAttribute("videoName", videoName)
+        return "video"
+    }
+
+    @GetMapping("/upload")
+    fun upload(): String {
+        return "upload"
+    }
+
+}
+
